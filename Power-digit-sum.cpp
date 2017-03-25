d58f095eb9b942c118ad8f6123cbea8747058406
ujjwal <ujjwal01kumar@gmail.com>
2017-03-26 03:16:40 +0530

    Easy (store large product in array)

---
+++ Power-digit-sum.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int fact(int x,vector<int> &v,int size){
+    int carry = 0,p;
+    for(int i=0;i<size;i++){
+        p = v[i]*x+carry;
+        v[i] = p%10;
+        carry=p/10;
+    }
+    while(carry){
+        v[size++] = carry%10;
+        carry/=10;
+    }
+    return size;
+}
+
+int sum(int n){
+    vector<int> v(10000);
+    v[0] = 1;
+    int size = 1;
+    for(int i=1;i<=n;i++)
+        size = fact(2,v,size);
+    int s = 0;
+    for(int i=0;i<size;i++)
+        s+=v[i];
+    
+    return s;
+}
+
+
+int main() {
+    
+    int t,n;cin>>t;
+    while(t--){
+        cin>>n;
+        cout<<sum(n)<<endl;
+    }
+    return 0;
+}
